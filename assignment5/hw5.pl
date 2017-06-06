ba090afb506058fd6fa46974b18e1b68eb03b358
Pranav Ramesh <rameshp@oregonstate.edu>
2017-06-06 09:25:16 -0700

    updated hw5, 2c needs to verified

--- assignment5/hw5.pl
+++ assignment5/hw5.pl
@@ -1,4 +1,3 @@
-
 /* Exercise 1 */
 
 when(275,10).
@@ -27,17 +26,17 @@ schedule(S, L, T) :- enroll(S, Z), where(Z, L), when(Z, T).
 
 usage(L, T) :- where(Z, L), when(Z, T).
 
-/* Part c*/
+/* Part c */
 
-conflict(L, T) :- when(Z, T), where(Z, L), when(W, T), where(W, L), W \= Z. 
+conflict(L, T) :- when(Z, T), where(Z, L), when(W, T), where(W, L), W \= Z.
 
-/* Part d Work in progress*/
+/* Part d */
 meet(A, B) :- schedule(A, L, T), schedule(B, L, T), A\=B, schedule(A, L, T1), schedule(B, L, T2), T1\==T2+1, A\=B.
 
-/* Exercise 2
-Part a*/
+/* Exercise 2 */
+/* Part a */
 
-rdup(L, M) :- rduop2(L, M).
+rdup(L, M) :- rdup2(L, M).
 rdup2([],[]).
 rdup2([H|T1], [H|T2]) :- rdup2(T1,T2) not(member(H, T1)).
 rdup2([H|T1], T2) :- rdup2(T1, T2), member(H, T1).
@@ -51,4 +50,9 @@ flat([H|T], L, F) :-
 flat(H, F, [H|F]) :- \+ is_list(H).
 
 /* Part c */
-project()
+/* Needs to be verified... */
+project([], _, [], _).
+project(_, [], [], _).
+project([I|J], [X|Y], [X|M], P) :- I =:= P, project(J, Y, M, P+1).
+project(J, [_|Y], M, P) :- project(J, Y, M, P+1).
+project(J, Y, M) :- project(J, Y, M, 1).
